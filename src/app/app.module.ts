8ed59abd2fdfa50dd99866ac32fadeabd0691d04
Davidov Pavel <davidov.pavel@gmail.com>
2020-07-09 13:11:17 +0300

    projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BuyComponent } from './buy/buy.component';
+import { CollaborationsComponent } from './collaborations/collaborations.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ExhibitionsComponent } from './exhibitions/exhibitions.component';
 import { FishComponent } from './fish.component';
-import { SubscribeComponent } from './subscribe/subscribe.component';
+import { LegaltermsComponent } from './legalterms/legalterms.component';
 import { LoginComponent } from './login/login.component';
 import { MetadataComponent } from './metadata/metadata.component';
-import { ExhibitionsComponent } from './exhibitions/exhibitions.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { CollaborationsComponent } from './collaborations/collaborations.component';
-import { BuyComponent } from './buy/buy.component';
-import { LegaltermsComponent } from './legalterms/legalterms.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { PublicationsComponent } from './publications/publications.component';
-import { VideoComponent } from './video/video.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { PublicationsComponent } from './publications/publications.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+import { VideoComponent } from './video/video.component';
 
 @NgModule({
   declarations: [
@@ -32,13 +33,10 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     ContactsComponent,
     PublicationsComponent,
     VideoComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
   ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
-  ],
+  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), AppRoutingModule, FlexLayoutModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
